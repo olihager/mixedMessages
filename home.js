b15b9a8a0bfa4d50c7b3c4076a1df9d5169a2ca2
olihager <ophager@gmail.com>
2023-02-14 16:58:08 -0300

    Added name and removed last line

--- home.js
+++ home.js
@@ -11,16 +11,19 @@ function message() {
         message2: [" road"," town"," house"
         ],
 
+        message3: ["Oliver", "Lorenzo", "Clara"
+        ],
+
         num: Math.floor(Math.random() * 3),
 
     }
 
     console.log(object.num);
 
-    console.log("Hello I am ! Today we will look at your horiscope");
-    console.log("It sounds like you need" + object.message1[object.num]);
-    console.log("You need to look for a" + object.message2[object.num]);
-    console.log("In this")
+    console.log(`Hello I am ${object.message3[object.num]}! Today we will look at your horiscope`);
+    console.log(`It sounds like you need ${object.message1[object.num]}`);
+    console.log(`You need to look for a ${object.message2[object.num]}`);
+    
      // end of object
 
 }
